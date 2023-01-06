78e33f821d1bb61d3388a012efaa228cd2013b09
Joeri Colman <colmanjoeri@msn.com>
2023-01-06 15:34:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"./webservice"
 )
 
 func main() {
+	fmt.Println("Starting server")
 	router := webservice.GetRouter()
 	http.ListenAndServe(":5000", router)
 }
